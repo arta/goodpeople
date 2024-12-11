c275e4e39e66b5b43d0d3d6bf931db20d12991c5
arta <martin@layeredcode.com>
2024-12-11 14:35:57 -0600

    fix: config; missing stylesheets in manifest.js
    % heroku logs -a goodpeople-staging --tail
    ..
    2024-12-11T20:11:49.440463+00:00 heroku[router]: at=info method=GET path="/assets/application.css.map" host=goodpeople-staging.herokuapp.com request_id=8bf2e6fc-6b9f-45d8-acc5-7d944c4c2d33 fwd="207.190.93.242" dyno=web.1 connect=0ms service=2ms status=404 bytes=1808 protocol=https
    2024-12-11T20:11:49.439088+00:00 app[web.1]: I, [2024-12-11T20:11:49.439049 #22]  INFO -- : [8bf2e6fc-6b9f-45d8-acc5-7d944c4c2d33] Started GET "/assets/application.css.map" for 207.190.93.242 at 2024-12-11 20:11:49 +0000
    2024-12-11T20:11:49.439503+00:00 app[web.1]: E, [2024-12-11T20:11:49.439480 #22] ERROR -- : [8bf2e6fc-6b9f-45d8-acc5-7d944c4c2d33]
    2024-12-11T20:11:49.439505+00:00 app[web.1]: [8bf2e6fc-6b9f-45d8-acc5-7d944c4c2d33] ActionController::RoutingError
    ..

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,5 @@
 //= link_tree ../images
-/* 12-11-24: Delete (or replace with //= link_directory ../stylesheets .scss ?)
-  //= link_directory ../javascripts .js
-*/
+//= link_directory ../stylesheets .css
 //= link_tree ../builds
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
