66a9899e765647135526094592d3d3887b6308d6
arta <martin@layeredcode.com>
2024-12-11 13:52:29 -0600

    fix (tiny): add empty .keep to app/assets/javascripts
    and comment out the loading line in the manifest.js for Heroku to compile
    (as an interim step; the directory will be deleted once migration from foundaiton-rails gem to importmap completed)

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,7 @@
 //= link_tree ../images
-//= link_directory ../javascripts .js
+/* 12-11-24: Delete (or replace with //= link_directory ../stylesheets .scss ?)
+  //= link_directory ../javascripts .js
+*/
 //= link_tree ../builds
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
