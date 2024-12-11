9dc2eed297c3e962d0065e6aaa7d3c2d0734efe4
arta <martin@layeredcode.com>
2024-12-11 13:20:53 -0600

    fix & style: move a script from layout file to js file
    and some clean up
    
    The JS script moved handles .current class in the nav menu

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,7 +6,23 @@ import Foundation from 'foundation-sites'
 window.jQuery = jquery
 window.$ = jquery
 
-// $(function () {
-//   $(document).foundation();
-// })
-$(document).foundation()
+// $(document).foundation()
+
+$(function () {
+  // Initialize Foundation
+  $(document).foundation();
+
+  // Custom script to add 'current' class to navigation links based on URL
+  // https://css-tricks.com/snippets/javascript/get-url-and-url-parts-in-javascript/
+  // https://css-tricks.com/snippets/jquery/add-active-navigation-class-based-on-url/
+  const loc = location.pathname.split("/").pop();
+  // console.log("Current location:", loc);
+  if (loc) {
+    const navLinks = $('nav a[href^="/' + loc + '"]');
+    // console.log(
+    //   "Navigation links found:", navLinks.length,
+    //   "Navigation links:", navLinks
+    // );
+    navLinks.parent().addClass('current');
+  }
+})
