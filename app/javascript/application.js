2e9c942234ddf45cf9527e385325484d1c5be143
arta <martin@layeredcode.com>
2024-12-22 13:45:22 -0600

    refactor(deps, js): modernize application.js
    • Foundation only uses window.$()
    : no need to attach .jQuery() since Foundation never uses it, and we won't either.
    
    • Old jQuery Document Ready wrapper
    
        $(function() { .. // code here .. })
    
    : or the equivalent:
    
        $(document).ready(function() { .. // code here .. })
    
    : is no longer needed because:
    - modern browsers load scripts with `defer` by default when using importmaps,
    - `javascript_importmap_tags` helper adds `defer` automatically,
    - `defer` ensures scripts run after DOM is loaded

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,28 +1,31 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Configure your import map in config/importmap.rb.
+// Read more: https://github.com/rails/importmap-rails
 
+// Named import of jQuery module
+// We need the jquery reference to assign it to a global $ variable (line 14)
 import jquery from 'jquery'
-import Foundation from 'foundation-sites'
 
-window.jQuery = jquery
+// Side-effect only import of Foundation module
+// Foundation attaches its plugins to jQuery (line 17)
+// We don't need exports from foundation-sites, Foundation reference in our code
+import 'foundation-sites'
+
+// Make jQuery available globally (as $) for Foundation's & our DOM manipulation
 window.$ = jquery
 
-// $(document).foundation()
+// Initialize all Foundation plugins
+$(document).foundation()
 
-$(function () {
-  // Initialize Foundation
-  $(document).foundation();
-
-  // Custom script to add 'current' class to navigation links based on URL
-  // https://css-tricks.com/snippets/javascript/get-url-and-url-parts-in-javascript/
-  // https://css-tricks.com/snippets/jquery/add-active-navigation-class-based-on-url/
-  const loc = location.pathname.split("/").pop();
-  // console.log("Current location:", loc);
-  if (loc) {
-    const navLinks = $('nav a[href^="/' + loc + '"]');
-    // console.log(
-    //   "Navigation links found:", navLinks.length,
-    //   "Navigation links:", navLinks
-    // );
-    navLinks.parent().addClass('current');
-  }
-})
+// Custom script to add 'current' class to navigation links based on URL
+// https://css-tricks.com/snippets/javascript/get-url-and-url-parts-in-javascript/
+// https://css-tricks.com/snippets/jquery/add-active-navigation-class-based-on-url/
+const loc = location.pathname.split("/").pop()
+// console.log("Current location:", loc)
+if (loc) {
+  const navLinks = $('nav a[href^="/' + loc + '"]')
+  // console.log(
+  //   "Navigation links found:", navLinks.length,
+  //   "Navigation links:", navLinks
+  // );
+  navLinks.parent().addClass('current')
+}
