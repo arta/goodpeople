05e8eb50d2d783546c9138f6f81f69240bec9180
arta <martin@layeredcode.com>
2025-09-14 21:18:10 -0500

    chore(rails): bump 7.2 -> 8.0
    % bin/rails app:update
    % bin/dev ☑️

--- config/puma.rb
+++ config/puma.rb
@@ -1,65 +1,26 @@
-# 7-25-25: Only use multiple workers in production (Heroku), not in development
-# (today's debugging note in thehouse has more info)
-# 7-22-25: updated w latest Rails & Heroku contents during Rails bump 7.1 -> 7.2
-# 1-22-20: override default config in favor of heroku's
-# https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server
-workers ENV['RAILS_ENV'] == 'development' ? 0 : Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
-threads threads_count, threads_count
+# 9-13-25: >>> Start :: pasted Heroku's recommended puma config for Rails apps
 
+# Processes count, allows better CPU utilization when executing Ruby code.
+workers(ENV.fetch('WEB_CONCURRENCY') { 2 })
+
+# Thread per process count allows context switching on IO-bound tasks for better CPU utilization.
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { 3 }
+threads(threads_count, threads_count)
+
+# Reduce memory usage on copy-on-write (CoW) systems.
 preload_app!
 
-# Support IPv6 by binding to host `::` instead of `0.0.0.0`
-port(ENV['PORT'] || 3000, "::")
+# Ruby buildpack sets RAILS_ENV and RACK_ENV in production.
+run_env = ENV["RAILS_ENV"] || ENV["RACK_ENV"] || "development"
+environment(run_env)
 
-# Turn off keepalive support for better long tails response time with Router 2.0
-# Remove this line when https://github.com/puma/puma/issues/3487 is closed, and the fix is released
-enable_keep_alives(false) if respond_to?(:enable_keep_alives)
+# Support IPv6 by binding to host `::` in production instead of `0.0.0.0` and `::1` instead of `127.0.0.1` in development.
+host = run_env == "production" ? "::" : "::1"
 
-rackup      DefaultRackup if defined?(DefaultRackup)
-environment ENV['RACK_ENV'] || 'development'
+# PORT environment variable is set by Heroku in production.
+port(ENV.fetch("PORT") { 3000 }, host)
 
-on_worker_boot do
-  # Worker-specific setup for Rails 4.1 to 5.2, after 5.2 it's not needed
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-  ActiveRecord::Base.establish_connection
-end
+# Allow Puma to be restarted by the `rails restart` command locally.
+plugin(:tmp_restart)
 
-# This configuration file will be evaluated by Puma. The top-level methods that
-# are invoked here are part of Puma's configuration DSL. For more information
-# about methods provided by the DSL, see https://puma.io/puma/Puma/DSL.html.
-
-# Puma starts a configurable number of processes (workers) and each process
-# serves each request in a thread from an internal thread pool.
-#
-# The ideal number of threads per worker depends both on how much time the
-# application spends waiting for IO operations and on how much you wish to
-# to prioritize throughput over latency.
-#
-# As a rule of thumb, increasing the number of threads will increase how much
-# traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
-#
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
-#
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
-# 7-22-25: Commented out in favor of Heroku's puma configuration.
-# threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
-# threads threads_count, threads_count
-
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-# 7-22-25: Commented out in favor of Heroku's puma configuration (atop)
-# port ENV.fetch("PORT", 3000)
-
-# Allow puma to be restarted by `bin/rails restart` command.
-# 7-22-25: Commented out in favor of Heroku's puma configuration (atop)
-# plugin :tmp_restart
-
-# Specify the PID file. Defaults to tmp/pids/server.pid in development.
-# In other environments, only set the PID file if requested.
-# 7-22-25: Commented out in favor of Heroku's puma configuration (atop)
-# pidfile ENV["PIDFILE"] if ENV["PIDFILE"]
+# 9-13-25: <<< End :: pasted Heroku's recommended puma config for Rails apps
