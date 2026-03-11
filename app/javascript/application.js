fe46ec5d16288b6ab7bbb99c7a87d6f0a8a1a240
arta <martin@layeredcode.com>
2026-03-11 13:23:50 -0500

    chore(deps): Foundation → CSS + Stimulus, WIP 3c/3
    Foundation JS fully replaced by Stimulus; jQuery was its only
    dependency here. Remove pins, vendor files, and dead code.
    
      - Remove foundation-sites and jquery pins from importmap.rb
      - Delete vendor/javascript/foundation-sites.js and jquery.js
      - Remove foundation-sites from package.json dependencies
      - Clean up commented-out Foundation/jQuery initialization
        block from application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,33 +1,5 @@
 // Configure your import map in config/importmap.rb.
 // Read more: https://github.com/rails/importmap-rails
 
-// Named import of jQuery module
-// We need the jquery reference to assign it to a global window.$ variable
-// import jquery from 'jquery'
-
-// Side-effect only import of Foundation module
-// Foundation attaches its plugins to jQuery: $(document).foundation()
-// We don't need exports from foundation-sites, Foundation reference in our code
-// import 'foundation-sites'
-
-// --- Initialization and global assignments ---
-
-// Make jQuery available globally (as $) for Foundation's DOM manipulation
-// window.$ = jquery
-
-// Initialize all Foundation plugins
-// $(document).foundation()
-// Caveat: $(document).foundation() initializes Foundation just once.
-// Turbo, by design, swaps pages without a full page reload, so Foundation
-// components will not re-init unless we hook into Turbo event(s) turbo:load
-// (and turbo:before-cache) to re-init (and clean up) them (respectively)
-// after each Turbo visit:
-// - Re-init Foundation on each Turbo load:
-// document.addEventListener("turbo:load", () => { $(document).foundation() })
-// - Prevent duplicate bindings (clean up) before Turbo caches the current page:
-// document.addEventListener("turbo:before-cache", () => {
-//   $(document).foundation("destroy")
-// })
-
 import "@hotwired/turbo-rails"
 import "controllers"
--- config/importmap.rb
+++ config/importmap.rb
@@ -6,8 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin 'jquery' # @3.7.1
-pin 'foundation-sites' # @6.9.0
 pin "@googlemaps/js-api-loader", to: "@googlemaps--js-api-loader.js" # @1.16.8
 
 pin 'clients-zip-data', to: 'clients-zip-data.js'
