80638872e1fc8300f73ff199875f4176987927f3
arta <martin@layeredcode.com>
2026-03-14 12:44:51 -0500

    refactor(js): extract nav close animation into helpers

--- app/javascript/controllers/nav_mobile_controller.js
+++ app/javascript/controllers/nav_mobile_controller.js
@@ -5,19 +5,35 @@ export default class extends Controller {
 
   connect() {
     if (sessionStorage.getItem("navCloseAnimDone")) {
-      this.navOffCanvasTarget.classList.add("nav-close-anim-done")
+      this.#lockNavCloseX()
     }
   }
 
   show() {
     this.navOffCanvasTarget.classList.add("is-shown")
     this.canvasOverlayTarget.classList.add("is-shown")
-    sessionStorage.setItem("navCloseAnimDone", "1")
+    this.#flagNavCloseAnim()
   }
 
   hide() {
     this.navOffCanvasTarget.classList.remove("is-shown")
     this.canvasOverlayTarget.classList.remove("is-shown")
+    this.#lockNavCloseX()
+  }
+
+
+
+  // ###########################################################################
+  // HELPERS (private to this controller)
+  // ---------------------------------------------------------------------------
+
+  // Limit mobile nav close burger [𝝣] into [x] animation to once per session
+
+  #flagNavCloseAnim() {
+    sessionStorage.setItem("navCloseAnimDone", "1")
+  }
+
+  #lockNavCloseX() {
     this.navOffCanvasTarget.classList.add("nav-close-anim-done")
   }
 }
