2c25e96b1c6aa146426bf85bab64e9d3c7ff9f6e
arta <martin@layeredcode.com>
2026-03-13 22:31:42 -0500

    feat(ui): animate mobile nav burger to X close button

--- app/javascript/controllers/nav_mobile_controller.js
+++ app/javascript/controllers/nav_mobile_controller.js
@@ -3,13 +3,21 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["navOffCanvas", "canvasOverlay"]
 
+  connect() {
+    if (sessionStorage.getItem("navCloseAnimDone")) {
+      this.navOffCanvasTarget.classList.add("nav-close-anim-done")
+    }
+  }
+
   show() {
     this.navOffCanvasTarget.classList.add("is-shown")
     this.canvasOverlayTarget.classList.add("is-shown")
+    sessionStorage.setItem("navCloseAnimDone", "1")
   }
 
   hide() {
     this.navOffCanvasTarget.classList.remove("is-shown")
     this.canvasOverlayTarget.classList.remove("is-shown")
+    this.navOffCanvasTarget.classList.add("nav-close-anim-done")
   }
 }
