fd73fdeb16a4ac9afb0ff13b6b73cfda83ff1cd9
arta <martin@layeredcode.com>
2026-03-30 11:07:52 -0400

    refactor(rails): simplify print page partials mapping

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -38,13 +38,13 @@ class StaticPagesController < ApplicationController
 
   def set_pages_for_print
     @pages_for_print = [
-      { title: 'Intro', link_action: :intro, partial: 'intro' },
-      { title: 'Care', link_action: :type_of_care, partial: 'type_of_care' },
-      { title: 'Caregivers', link_action: :caregivers, partial: 'caregivers' },
-      { title: 'Procedure', link_action: :procedure, partial: 'procedure' },
-      { title: 'Service area', link_action: :servicearea, partial: 'servicearea' },
-      { title: 'Cost', link_action: :cost, partial: 'cost' },
-      { title: 'Contact us', link_action: :contactus, partial: 'contactus' }
+      { title: 'Intro', action: 'intro' },
+      { title: 'Care', action: 'type_of_care' },
+      { title: 'Caregivers', action: 'caregivers' },
+      { title: 'Procedure', action: 'procedure' },
+      { title: 'Service area', action: 'servicearea' },
+      { title: 'Cost', action: 'cost' },
+      { title: 'Contact us', action: 'contactus' }
     ]
   end
 end
